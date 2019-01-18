bd2bca801c0a4d06fffb1e4c291b0bcec38985b3
Katherine Aguilera <katherinam35@gmail.com>
2019-01-18 16:55:53 -0500

    configurando un stack navigator

--- App.js
+++ App.js
@@ -9,10 +9,36 @@ import About from'./src/screens/about';
 import Profile from'./src/screens/profile';
 
 //rutas y configuracion
-const AppNavigator = createStackNavigator({
-  // Home: Home
-  Home:Home, Login:Login, Profile:Profile, About:About,
-});
+// Home:Home,
+// Login:Login,
+// Profile:Profile,
+// About:About,
+const AppNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: Home,
+      path: 'home/',
+      // titulo
+      navigationOptions: {
+        title: 'Esta es la Home'
+      }
+    },
+    About,
+    Profile,
+    Login,
+  },
+  {
+    // donde inicia la app
+    initialRouteName: 'About',
+    defaultNavigationOptions: {
+      title: 'Un titulo genérico'
+    },
+    initialRouteKey: 'login',
+    initialRouteParams: {
+      nombre: 'Leonidas Esteban'
+    }
+  }
+);
 const AppContainer = createAppContainer(AppNavigator);
 
 export default AppContainer;
